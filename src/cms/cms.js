9141143c3e8c7bc40b17c6a9a291d2669f7fa017
Roy Tanaka <tanaka@mac.com>
2021-12-13 08:18:26 -0500

    uninstalls cloudinary and uploadcare

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,6 +1,6 @@
 import CMS from 'netlify-cms-app'
-import uploadcare from 'netlify-cms-media-library-uploadcare'
-import cloudinary from 'netlify-cms-media-library-cloudinary'
+// import uploadcare from 'netlify-cms-media-library-uploadcare'
+// import cloudinary from 'netlify-cms-media-library-cloudinary'
 
 import AboutPagePreview from './preview-templates/AboutPagePreview'
 import BlogPostPreview from './preview-templates/BlogPostPreview'
@@ -9,8 +9,8 @@ import LessonPostPreview from './preview-templates/LessonPostPreview'
 import ProductPagePreview from './preview-templates/ProductPagePreview'
 import IndexPagePreview from './preview-templates/IndexPagePreview'
 
-CMS.registerMediaLibrary(uploadcare)
-CMS.registerMediaLibrary(cloudinary)
+// CMS.registerMediaLibrary(uploadcare)
+// CMS.registerMediaLibrary(cloudinary)
 
 CMS.registerPreviewTemplate('index', IndexPagePreview)
 CMS.registerPreviewTemplate('about', AboutPagePreview)
